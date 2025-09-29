43c1dd5bdd9fa286daf5ce9df10e5bfade169923
Varvara Kozhevina <varvarakozhevina@mbp-de-varvara.home>
2025-09-29 23:42:50 +0200

    ajout fonction mb_push_line

--- src/parse_map.c
+++ src/parse_map.c
@@ -109,3 +109,54 @@ static bool mb_grow_buf(t_mapbuild *map, size_t need_more)
     map->capacity = new_cap;
     return (true);
 }
+
+bool	mb_push_line(t_mapbuild *map, char *line)
+{
+	int		line_len;
+	int		i;
+	int		players_nb;
+	int		player_column;
+	size_t	need_more;
+
+	line_len = line_len_no_nl(line);
+	i = 0;
+	players_nb = 0;
+	player_column = -1;
+
+	if (line_len == 0)
+		return (false);
+	while (i < line_len)
+	{
+		unsigned char c;
+		c = (unsigned char)line[i];
+		if (!is_map_char(c))
+			return (false);
+		if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		{
+			if (map->player_count != 0)   // un joueur déjà vu sur une ligne précédente
+				return (false);
+			players_nb ++;
+			if (players_nb > 1)           // 2-ieme joueur sur la même ligne
+				return (false);
+			player_column = i;
+		}
+		i++;
+	}
+	need_more = (size_t)line_len + 1;
+	if (!mb_grow_buf(map, need_more))
+		return (false);
+	ft_memcpy(map->buf + map->len, line, (size_t)line_len); // copier line_len octets a la fin des donnees deja copiées précédemment
+	map->len += (size_t)line_len; // on met a jour le buff
+	map->buf[map->len + 1] = '\n';
+	if (line_len > map->maxw)
+		map->maxw = line_len; // on met a jour la ligne la plus longue (largeur max)
+	if (players_nb == 1)
+	{
+		map->player.row = map->rows;
+		map->player.column = player_column;
+		map->player.dir = line[player_column];
+		map->player_count += 1;
+	}
+	map->rows++; // push réussi, on a stocke une ligne de plus
+	return (true);
+}
